a4cd411b5f872782a930d3bc3cd2eb464fbf5de4
ryanishere212 <arianakarbasi@gmail.com>
2026-02-13 22:17:09 +0330

    homeworks
    (1.py) contains Variables / Input and Output and Operators
    (2.py) contains Conditional statements

---
+++ 1.py
@@ -0,0 +1,50 @@
+#3
+
+#x=int(input('please enter the first number:')) 
+#y=int(input("please enter the secound number:"))
+#print(x+y, x-y, x*y, x/y )
+
+#4
+#a=int(12.5)
+#b=int(15.7)
+#c=int(5.98)
+
+#print(a,b,c,sep="*")
+
+#5
+#print(f"{a}*{b}*{c}")
+#print("{}*{}*{}".format(a,b,c))
+#print(a,"*",b,"*",c)
+
+#6
+#x=input('enter num 1')
+#y=input('enter num 2')
+#z=input('enter num 3')
+
+#m=x+y+z/3
+#print(m)
+
+#7
+#r=input('the raduce of circle:')
+#pi=3.14
+#A=pi*r*r
+#P=2*pi*r
+
+#print(A,P)
+
+#8
+#Age=int(input('please enter your age in year:'))
+#AgeInMOnth=(Age*12)
+#AgeInDays=(Age*365)
+#AgeInHours=(AgeInDays*24)
+#print(f"ypu have lived for {AgeInMOnth} months! \nand {AgeInDays} days!! \nand {AgeInHours} hours till now!!! ")
+
+#9
+#x=int(input('please enter a two-digit number:'))
+#FirstDigit=x//10
+#SecDigit=x%10
+#Sum=FirstDigit+SecDigit
+#Reverse=str(SecDigit)+str(FirstDigit)
+#print(f"the sum of digits is {Sum} \nthe reverse of the digit is {Reverse}")
+
+#10
---
+++ 2.py
@@ -0,0 +1,107 @@
+#1
+#x=int(input("please enter your number:"))
+#if x==0:
+#    print("your number is zero!")
+#elif x%2==0:
+#    print("your number is even!")
+#else:
+#    print("your number is odd!")
+
+#3
+#FG=int(input("please enter your first garde:" ))
+#SG=int(input("please enter your first garde:" ))
+#TG=int(input("please enter your first garde:" ))
+#FG=int(input("please enter your first garde:" ))
+#Sum=(FG+SG+TG+FG)/4
+#if Sum>18:
+#    print("A")
+#elif Sum>16:
+#    print("B")
+#elif Sum>12:
+#    print("C")
+#elif Sum>10:
+#    print("D")
+#elif Sum<10:
+#    print("Failed")
+    
+    
+#4
+#Player1=input("player1 turn (Scissors/Paper/Rock) ")
+#Player2=input("player2 turn (Scissors/Paper/Rock) ")
+#if Player1==Player2:
+#    print('Raw!')
+#elif Player1=="Scissors":
+#    if Player2=="Rock":
+#        print("Player2 has won!")
+#    elif Player2=="Paper":
+#        print("Player1 has won!")
+#elif Player1=='Rock':
+#    if Player2=='Scissors':
+#        print("Player1 has won!")
+#    elif Player2=="Paper":
+#        print("Player2 has won!")
+#elif Player1=="Paper":
+#    if Player2=='Scissors':
+#        print("Player2 has won!")
+#    elif Player2=='Rock':
+#        print("Player1 has won!")
+
+
+
+
+#5
+#num1=int(input("plrase enter num1"))
+#op=input("please enter an operator")
+#num2=int(input("please enter num2"))
+#if op=="+":
+#    print(f"{num1}{op}{num2} = {num1+num2}")
+#elif op=="-":
+#     print(f"{num1}{op}{num2} = {num1-num2}")
+#elif op=="*":
+#     print(f"{num1}{op}{num2} = {num1*num2}")
+#elif op=="/":
+#    if num2==0:
+#        print("num2 cannot be zero!")
+#    else:
+#         print(f"{num1}{op}{num2} = {num1/num2}")
+#else:
+#    print("invalid operator")    
+    
+    
+#6
+#x=int(input("enter number:"))
+#if x==0:
+#    print(x)
+#elif x%15==0:
+#    if x%5==0 and x%3==0:
+#        print("Fizzbuzz!")
+#    elif x%3==0:
+#        print("Fizz!")
+#    elif x%5==0:
+#        print("Buzz!")
+#elif x%5==0:
+#    print("Buzz!")
+#elif x%3==0:
+#    print("Fizz!")
+#else:
+#    print(x)   
+
+
+#7
+#x=int(input("enter a number by player1:"))
+#n=int(input("enter a number by player2:"))
+#if n==x:
+#    print('player2 has won!')
+#else:
+#    print("player1 has won!")
+
+
+#8
+#Temp=int(input('enter the temperature:'))
+#if Temp>=100:
+#    print("vaporized")
+#elif Temp<=0:
+#    print("Frezzed")
+#elif Temp<=100:
+#    print("liquid")
+
